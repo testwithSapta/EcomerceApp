4edcc6f9757cfec43170135a10f24e89b8c2df72
testwithSapta <saptamita43@gmail.com>
2024-02-12 12:01:01 +0530

    This is an ecommerce app

---
+++ api/index.js
@@ -0,0 +1,5 @@
+const exprsde - -ess = require("express");
+const bodyn= require("body-parse");
+const mongose = require("nongoose")
+const crypto = require("crypto")
+ const nodemailer = require("nodemailer")
